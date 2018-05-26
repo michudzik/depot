ab8e0438d971afd73a7b5e3c17ea85f32526193e
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-05-26 23:56:14 +0200

    Fix code intendation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,17 @@
 class Product < ApplicationRecord
-	validates :title, 			presence: true,
-													uniqueness: true,
-													length: { minimum: 10 }
+	validates :title, 	presence: true,
+						uniqueness: true,
+						length: { minimum: 10 }
 
 	validates :description, presence: true
 
-	validates :image_url,		presence: true,
-													#allow_blank: true,
-													format: {
-														with: 		%r{\.(png|jpg|gif)\Z}i,
-														message: 	'must be a URL for GIF, JPG or PNG image'
-													}
+	validates :image_url,	presence: true,
+							#allow_blank: true,
+							format: {
+								with: 		%r{\.(png|jpg|gif)\Z}i,
+								message: 	'must be a URL for GIF, JPG or PNG image'
+							}
 
-	validates :price,				numericality: { greater_than_or_equal_to: 0.01 }
+	validates :price,	numericality: { greater_than_or_equal_to: 0.01 }
 end
 
