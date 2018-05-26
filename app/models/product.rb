cb26ea0acd09dc08f6c1730e92cf75e1d7e9ff84
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-05-26 14:01:10 +0200

    Finish validation for product data model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+	validates :title, 			presence: true,
+													uniqueness: true
+
+	validates :description, presence: true
+
+	validates :image_url,		presence: true,
+													#allow_blank: true,
+													format: {
+														with: 		%r{\.(png|jpg|gif)\Z}i,
+														message: 	'must be a URL for GIF, JPG or PNG image'
+													}
+
+	validates :price,				numericality: { greater_than_or_equal_to: 0.01}
 end
+
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,91 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	# Specify which fitures to load
+	fixtures :products
+
+	def setup
+		@product = products(:ruby)
+	end
+
+	test "product attributes should not be empty" do
+		product = Product.new
+		assert product.invalid?
+		assert product.errors[:title].any?
+		assert product.errors[:description].any?
+		assert product.errors[:image_url].any?
+		assert product.errors[:price].any?
+	end
+
+	test "product should be valid" do
+		assert @product.valid?
+	end
+
+	test "title should be present" do
+		@product.title = "    "
+		assert @product.invalid?
+		assert_equal @product.errors[:title], ["can't be blank"]
+	end
+
+	test "description should be present" do
+		@product.description = "    "
+		assert @product.invalid?
+		assert_equal @product.errors[:description], ["can't be blank"]
+	end
+
+	test "image url should be present" do
+		@product.image_url = nil
+		assert @product.invalid?
+		assert_equal @product.errors[:image_url], ["can't be blank", "must be a URL for GIF, JPG or PNG image"]
+	end
+
+	test "price should not be negative" do
+		@product.price = -0.01
+		assert @product.invalid?
+		assert_equal @product.errors[:price], ["must be greater than or equal to 0.01"]
+	end
+
+	test "price should not be zero" do
+		@product.price = 0.00
+		assert @product.invalid?
+		assert_equal @product.errors[:price], ["must be greater than or equal to 0.01"]
+	end
+
+	test "price should be valid" do
+		@product.price = 0.01
+		assert @product.valid?
+		assert @product.errors[:price].empty?
+	end
+
+	test "title should be unique" do
+		product = Product.new(title: @product.title,
+													description: 'description',
+													image_url: 'whatever.jpg',
+													price: 30.00)
+		assert product.invalid?
+		assert_equal product.errors[:title], ["has already been taken"]
+	end
+
+	test "image_url should be valid" do
+		valid = %w{ fred.jpg fred.gif fred.png
+							  FRED.JPG Fred.jpg
+							  http://a.b.c/x/y/z/fred.gif }
+
+		valid.each do |image|
+			@product.image_url = image
+			assert @product.valid?
+			assert @product.errors[:image_url].empty?
+		end
+	end
+
+	test "image_url should be invalid" do
+		invalid = %w{ fred.doc fred.gif/more fred.gif.more }
+
+		invalid.each do |image|
+			@product.image_url = image
+			assert @product.invalid?
+			assert_equal @product.errors[:image_url], ["must be a URL for GIF, JPG or PNG image"]
+		end
+	end
+
 end
