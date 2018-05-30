aa162ddcd263f74d72de116395a603a3ab08298e
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-05-30 15:09:51 +0200

    Add simple cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, presence: true
   validates :title,	uniqueness: true
   validates :title,	length: { minimum: 10 }
@@ -9,6 +12,17 @@ class Product < ApplicationRecord
                           with: 		%r{\.(png|jpg|gif)\Z}i,
                           message: 	'must be a URL for GIF, JPG or PNG image'
                         }
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01,  }
+
+  private
+
+    # Ensure that not a single line item references that product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
+    
 end
 
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -56,4 +56,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+  test "should not destroy product" do
+    assert_no_difference('Product.count') do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
 end
