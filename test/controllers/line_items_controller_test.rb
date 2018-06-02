11ef8bb4d40e1f74b0329b8a69b3d00d88a0b30f
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-06-02 12:48:28 +0200

    Add highlighting when a new item is added to the cart

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -15,6 +15,15 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:ruby).id }, xhr: true
+    end
+
+    assert_response :success
+    assert_match /<tr class=\\"line-item-highlight/, @response.body
+  end
+
   test "should create line_item" do
     assert_difference('LineItem.count') do
       post line_items_url, params: { product_id: products(:ruby).id }
