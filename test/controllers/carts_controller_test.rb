abd065ff367610c430f8ed51d28666ca5aba2a94
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-06-03 13:37:12 +0200

    Add destroying cart via ajax

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -49,4 +49,14 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to store_index_url
   end
+
+  test "should destroy cart via ajax" do
+    post line_items_url, params: { product_id: products(:one).id }
+    @cart = Cart.find(session[:cart_id])
+    assert_difference('Cart.count', -1) do
+      delete cart_url(@cart), xhr: true
+    end
+
+    assert_response :success
+  end
 end
